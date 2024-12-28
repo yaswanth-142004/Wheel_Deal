28c93c6c5523a12a52435a6bd9ae69be30d7a678
Yaswanth V <yashwanth142004@gmail.com>
2024-12-28 19:32:37 +0530

    Added routes and added authentication

---
+++ Wheel_Deal/src/Home.jsx
@@ -0,0 +1,15 @@
+import { SignInButton } from '@clerk/clerk-react'
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const Home = () => {
+  return (
+    <div>
+        <SignInButton mode = 'modal' forceRedirectUrl='/' >
+      <Button>Sign In</Button>
+      </SignInButton >
+    </div>
+  )
+}
+
+export default Home
--- Wheel_Deal/src/main.jsx
+++ Wheel_Deal/src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Home'
+import Contact from './Contact'
+import {ClerkProvider} from '@clerk/clerk-react'
+
+const router = createBrowserRouter([
+  {
+    path: '/',element: <Home/>
+
+  },
+  {
+    path : '/contact',
+    element: <Contact/>
+  },
+  {
+
+  }
+
+])
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+<RouterProvider router ={router} />
+    </ClerkProvider>
+
+    
+    
   </StrictMode>,
 )
